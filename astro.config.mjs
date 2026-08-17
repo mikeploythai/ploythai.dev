33a5897790cb9aff32aff76e388b44105e778e52
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2026-08-16 20:29:51 -0700

    revalidate page data hourly, fix iOS landscape text inflation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
+import { cacheCloudflare } from "@astrojs/cloudflare/cache";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	adapter: cloudflare(),
+	cache: {
+		provider: cacheCloudflare(),
+	},
 });
